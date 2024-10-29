e15d0b28b8c4104a2dc66cfa037bdd0d55dcc487
ngryan5 <165727909+ngryan5@users.noreply.github.com>
2024-10-29 05:27:07 -0700

    Update calculator.hpp

---
+++ calculator.hpp
@@ -0,0 +1,14 @@
+/*
+ * DO NOT MODIFY THIS FILE IN ANY WAY
+ */
+
+#ifndef CALCULATOR_HPP
+#define CALCULATOR_HPP
+
+#include <string>
+
+bool is_valid_expression(std::string expression);
+
+double compute_value(std::string expression);
+
+#endif
